5a2cf837bd7fec0fa31ffe02df32bc41ff92128e
skillsapphire <skillsapphire@gmail.com>
2021-04-04 17:15:38 +0200

    added logging

--- src/main/java/com/asb/example/SpringBootJpaCrudExampleApplication.java
+++ src/main/java/com/asb/example/SpringBootJpaCrudExampleApplication.java
@@ -1,13 +1,18 @@
 package com.asb.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJpaCrudExampleApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootJpaCrudExampleApplication.class);
 	public static void main(String[] args) {
+		LOGGER.info("hello world");
 		SpringApplication.run(SpringBootJpaCrudExampleApplication.class, args);
 	}
+
 
 }
